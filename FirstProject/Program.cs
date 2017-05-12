c9717e56ec0d8a51364d42ac05008e9a98a7289f
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-05-11 19:50:46 -0600

    Added a few lines

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,6 +7,13 @@ namespace FirstProject
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello Xamarin University!");
+
+            string name = "Judy";
+            string city = "Phoenix";
+            int age = 25;
+
+            Console.WriteLine("My name is " + name + " and I live in " + city + " and I am " + age + " years old.");
+            Console.WriteLine("My name is {0} and I live in {1} and I am {2} years old.", name, city, age);
         }
     }
 }
