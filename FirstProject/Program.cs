ff035a3ee6414c4d7a877ef5dd158f9b19151179
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-05-11 18:50:00 -0600

    Initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin University!");
+        }
+    }
+}
